1b71951b86a089090400b13ab156f3d1756b1ec5
konufer <115653609+konufer@users.noreply.github.com>
2022-12-06 05:07:31 -0500

    Nothing to See Here

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensor.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+public class ColorSensor {
+    public static ColorSensor color;
+
+    public ColorSensor(ColorSensor color){
+        this.color = color;
+    }
+}
